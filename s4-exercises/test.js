7a567a274e2b919b52a762b6c1152b7b567b0a50
SilvirD <bupbi99@gmail.com>
2021-03-12 19:17:00 +0700

    2nd commit

---
+++ s4-exercises/test.js
@@ -0,0 +1,7 @@
+var person = { name: "Sarah", country: "Nigeria", job: "Developer" };
+
+var { name, country, job } = person;
+
+console.log(name); //"Sarah"
+console.log(country); //"Nigeria"
+console.log(job); //Developer"
